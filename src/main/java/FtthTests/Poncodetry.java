6a7487404fa308ed30bb6009535f69af5a3865b5
mertdogan <mertcandogan@windowslive.com>
2021-04-14 10:27:56 +0300

    code try

--- src/main/java/FtthTests/Poncodetry.java
+++ src/main/java/FtthTests/Poncodetry.java
@@ -31,7 +31,7 @@ public class Poncodetry extends TestBase {
             Thread.sleep(2000);
             driver.get("https://10.98.42.82:32500/dashboard");
             wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Cih Sorgulama")));
-            driver.findElement(By.linkText("Cih Sorgulama")).click();
+            driver.findElement(By.linkText("Cihaz Sorgulama")).click();
             wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Pon")));
             driver.findElement(By.linkText("Pon")).click();
             Thread.sleep(2500);
@@ -39,7 +39,7 @@ public class Poncodetry extends TestBase {
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='root']/div[2]/div[2]/div/div/main/div/div/div/div/div/div/div[2]/div/div[2]/table/tbody/tr[2]/td/button/span")));
             driver.findElement(By.xpath("//div[@id='root']/div[2]/div[2]/div/div/main/div/div/div/div/div/div/div[2]/div/div[2]/table/tbody/tr[2]/td/button/span")).click();
             Thread.sleep(2500);
-            assertEquals("Alt Cihaz ", driver.findElement(By.id("pr_id_5_header")).getText());
+            assertEquals("Alt Cihaz Sayısı", driver.findElement(By.id("pr_id_5_header")).getText());
 
 
             } catch (Exception e) {
